a6a9e7dcc29fcf0e91a8d5837f80c9edd72247ee
p01s0n.ivy <ivana_lim@dlsu.edu.ph>
2018-02-19 18:11:27 +0800

    Revert "added splash screen"
    This reverts commit 3c941cf50ed4e6e45b9b692e041df0524de3ea86.

--- app/src/main/java/com/werelit/neurolls/neurolls/MainActivity.java
+++ app/src/main/java/com/werelit/neurolls/neurolls/MainActivity.java
@@ -1,8 +1,6 @@
 package com.werelit.neurolls.neurolls;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -15,25 +13,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import sun.applet.Main;
-
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(homeIntent);
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
