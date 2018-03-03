0ff4c4dea57d2bf1ed6e7221cc3e57bab9acb2ae
p01s0n.ivy <ivana_lim@dlsu.edu.ph>
2018-03-03 14:39:40 +0800

    updated QueryUtils

--- app/src/main/java/com/werelit/neurolls/neurolls/api/MediaTaskLoader.java
+++ app/src/main/java/com/werelit/neurolls/neurolls/api/MediaTaskLoader.java
@@ -1,4 +1,51 @@
 package com.werelit.neurolls.neurolls.api;
 
-public class MediaTaskLoader {
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import com.werelit.neurolls.neurolls.model.Media;
+
+import java.util.List;
+
+/**
+ * Loads a list of medias by using an AsyncTask to perform the
+ * network request to the given URL.
+ */
+public class MediaLoader extends AsyncTaskLoader<List<Media>> {
+
+    /** Tag for log messages */
+    private static final String LOG_TAG = MediaLoader.class.getName();
+
+    /** Query URL */
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link MediaLoader}.
+     *
+     * @param context of the activity
+     * @param url to load data from
+     */
+    public MediaLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    /**
+     * This is on a background thread.
+     */
+    @Override
+    public List<Media> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+
+        // Perform the network request, parse the response, and extract a list of medias.
+        List<Media> medias = QueryUtils.fetchMediaData(mUrl);
+        return medias;
+    }
 }
--- app/src/main/java/com/werelit/neurolls/neurolls/api/QueryUtils.java
+++ app/src/main/java/com/werelit/neurolls/neurolls/api/QueryUtils.java
@@ -1,22 +1,41 @@
 package com.werelit.neurolls.neurolls.api;
-
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.werelit.neurolls.neurolls.model.Film;
+import com.werelit.neurolls.neurolls.model.Media;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
-public class QueryUtils {
+/**
+ * Helper methods related to requesting and receiving media data from USGS.
+ */
+public final class QueryUtils {
 
     private static final String TAG = QueryUtils.class.getSimpleName();
 
-    private QueryUtils(){}
+    /**
+     * Create a private constructor because no one should ever create a {@link QueryUtils} object.
+     * This class is only meant to hold static variables and methods, which can be accessed
+     * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
+     */
+    private QueryUtils() {
+    }
 
+    // TODO: @Yuta this method is from your QueryUtils.
     public static List<Film> testExtractFilm(){
         List<Film> films = new ArrayList<>();
 
@@ -35,6 +54,179 @@ public class QueryUtils {
         }
 
         return films;
+    }
+
+    /**
+     * Query the USGS dataset and return a list of {@link com.werelit.neurolls.neurolls.model.Media} objects.
+     */
+    public static List<Media> fetchMediaData(String requestUrl) {
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Create URL object
+        URL url = createUrl(requestUrl);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(TAG, "Problem making the HTTP request.", e);
+        }
+
+        // Extract relevant fields from the JSON response and create a list of {@link Media}s
+        List<Media> medias = extractFeatureFromJson(jsonResponse);
+
+        // Return the list of {@link Media}s
+        return medias;
+    }
+
+
+    /**
+     * Return a list of {@link Media} objects that has been built up from
+     * parsing the given JSON response.
+     */
+    private static List<Media> extractFeatureFromJson(String mediaJSON) {
+        // If the JSON string is empty or null, then return early.
+        if (TextUtils.isEmpty(mediaJSON)) {
+            return null;
+        }
+
+        // Create an empty ArrayList that we can start adding medias to
+        List<Media> medias = new ArrayList<>();
+
+        // TODO: Parse the response given by the SAMPLE_JSON_RESPONSE string and build up a list of Media objects with the corresponding data.
+        // Try to parse the JSON response string. If there's a problem with the way the JSON
+        // is formatted, a JSONException exception object will be thrown.
+        // Catch the exception so the app doesn't crash, and print the error message to the logs.
+        /*
+        try {
+            // Create a JSONObject from the JSON response string
+            JSONObject baseJsonResponse = new JSONObject(mediaJSON);
+
+            // Extract the JSONArray associated with the key called "features",
+            // which represents a list of features (or medias).
+            JSONArray mediaArray = baseJsonResponse.getJSONArray("features");
+
+            // For each media in the mediaArray, create an {@link Media} object
+            for (int i = 0; i < mediaArray.length(); i++) {
+
+                // Get a single media at position i within the list of medias
+                JSONObject currentMedia = mediaArray.getJSONObject(i);
+
+                // For a given media, extract the JSONObject associated with the
+                // key called "properties", which represents a list of all properties
+                // for that media.
+                JSONObject properties = currentMedia.getJSONObject("properties");
+
+                // Extract the value for the key called "mag"
+                double magnitude = properties.getDouble("mag");
+
+                // Extract the value for the key called "place"
+                String location = properties.getString("place");
+
+                // Extract the value for the key called "time"
+                long time = properties.getLong("time");
+
+                // Extract the value for the key called "url"
+                String url = properties.getString("url");
+
+                // Create a new {@link Media} object with the magnitude, location, time,
+                // and url from the JSON response.
+                Media media = new Media(magnitude, location, time, url);
+
+                // Add the new {@link Media} to the list of medias.
+                medias.add(media);
+            }
+
+        } catch (JSONException e) {
+            // If an error is thrown when executing any of the above statements in the "try" block,
+            // catch the exception here, so the app doesn't crash. Print a log message
+            // with the message from the exception.
+            Log.e("QueryUtils", "Problem parsing the media JSON results", e);
+        }
+        */
+        // Return the list of medias
+        return medias;
+    }
+
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "Problem building the URL ", e);
+        }
+        return url;
+    }
+
+    /**
+     * Make an HTTP request to the given URL and return a String as the response.
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Problem retrieving the media JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // Closing the input stream could throw an IOException, which is why
+                // the makeHttpRequest(URL url) method signature specifies than an IOException
+                // could be thrown.
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server.
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
     }
 
 }
