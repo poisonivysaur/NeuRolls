4076dbfc3e88b636dd691858140d08ed7b9fc135
p01s0n.ivy <ivana_lim@dlsu.edu.ph>
2018-03-03 14:52:00 +0800

    added view film detail activity

---
+++ app/src/main/java/com/werelit/neurolls/neurolls/ViewFilmDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.werelit.neurolls.neurolls;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class ViewFilmDetailsActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_film_detail);
+    }
+}
