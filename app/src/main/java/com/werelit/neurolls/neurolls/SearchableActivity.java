9e2b6ac735458557962800f9cee89741eaab2060
p01s0n.ivy <ivana_lim@dlsu.edu.ph>
2018-03-04 10:41:56 +0800

    added searchable activity

--- app/src/main/java/com/werelit/neurolls/neurolls/SearchableActivity.java
+++ app/src/main/java/com/werelit/neurolls/neurolls/SearchableActivity.java
@@ -1,7 +1,24 @@
 package com.werelit.neurolls.neurolls;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class SearchableActivity extends AppCompatActivity{
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.entertainment_roll);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //doMySearch(query);
+            Toast.makeText(this, query, Toast.LENGTH_SHORT).show();
+        }
+    }
 }
