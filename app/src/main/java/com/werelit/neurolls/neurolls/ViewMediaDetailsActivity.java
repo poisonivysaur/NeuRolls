c59131312e15366a4a447aab9b7d561cb9c39a32
p01s0n.ivy <ivana_lim@dlsu.edu.ph>
2018-03-04 14:59:15 +0800

    added internal comments

--- app/src/main/java/com/werelit/neurolls/neurolls/ViewMediaDetailsActivity.java
+++ app/src/main/java/com/werelit/neurolls/neurolls/ViewMediaDetailsActivity.java
@@ -22,58 +22,81 @@ public class ViewMediaDetailsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // get intent that was passed when the recycler view item was pressed
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        if (bundle != null) { // there is a bundle
-            int mediaCategory = bundle.getInt(MediaKeys.MEDIA_CATEGORY_KEY);
+        if (bundle != null) { // check first if the bundle is not empty
+
+            // get all the common attributes of the 3 categories of media
             String mediaName = bundle.getString(MediaKeys.MEDIA_NAME_KEY);
             String mediaGenre = bundle.getString(MediaKeys.MEDIA_GENRE_KEY);
             int mediaYear = bundle.getInt(MediaKeys.MEDIA_YEAR_KEY, 0);
 
+            // get the the media category (depending which type of recycler view item was pressed)
+            // this only applies for the view ALL media fragment but neverthe less this activity
+            // will be used for all the other fragments for each type of category
+            int mediaCategory = bundle.getInt(MediaKeys.MEDIA_CATEGORY_KEY);
+
+            // if the recycler view item pressed is a film,
             if (mediaCategory == CategoryAdapter.CATEGORY_FILMS) {
+
+                // set the content of the detail to the film detail xml layout then
+                // get the views of that view group
                 setContentView(R.layout.view_film_detail);
                 TextView duration = (TextView) findViewById(R.id.duration);
                 TextView director = (TextView) findViewById(R.id.director);
                 TextView production = (TextView) findViewById(R.id.production);
                 TextView synopsis = (TextView) findViewById(R.id.synopsis);
 
+                // get the attributes for a Film object
                 int filmDuration = bundle.getInt(MediaKeys.FILM_DURATION_KEY);
                 String filmDirector = bundle.getString(MediaKeys.FILM_DIRECTOR_KEY);
                 String filmProduction = bundle.getString(MediaKeys.FILM_PRODUCTION_KEY);
                 String filmSynopsis = bundle.getString(MediaKeys.FILM_SYNOPSIS_KEY);
 
+                // set the views of the xml layout to the attribute values
                 duration.setText("" + filmDuration);
                 director.setText(filmDirector);
                 production.setText(filmProduction);
                 synopsis.setText(filmSynopsis);
 
-            }else if(mediaCategory == CategoryAdapter.CATEGORY_BOOKS) {
+            }
+            // if the recycler view item pressed is a book,
+            else if(mediaCategory == CategoryAdapter.CATEGORY_BOOKS) {
+                // set the content of the detail to the book detail xml layout then
+                // get the views of that view group
                 setContentView(R.layout.view_book_detail);
                 TextView author = (TextView) findViewById(R.id.author);
                 TextView publisher = (TextView) findViewById(R.id.publisher);
                 TextView description = (TextView) findViewById(R.id.description);
 
+                // get the attributes for a Book object
                 String bookAuthor = bundle.getString(MediaKeys.BOOK_AUTHOR_KEY);
                 String bookPublisher = bundle.getString(MediaKeys.BOOK_PUBLISHER_KEY);
                 String bookDescription = bundle.getString(MediaKeys.BOOK_DESCRIPTION_KEY);
 
+                // set the views of the xml layout to the attribute values
                 author.setText(bookAuthor);
                 publisher.setText(bookPublisher);
                 description.setText(bookDescription);
 
             }else if(mediaCategory == CategoryAdapter.CATEGORY_GAMES) {
 
+                // set the content of the detail to the game detail xml layout then
+                // get the views of that view group
                 setContentView(R.layout.view_game_detail);
                 TextView platform = (TextView) findViewById(R.id.platform);
                 TextView publisher = (TextView) findViewById(R.id.publisher);
                 TextView series = (TextView) findViewById(R.id.series);
                 TextView storyline = (TextView) findViewById(R.id.storyline);
 
+                // get the attributes for a Game object
                 String gamePlatform = bundle.getString(MediaKeys.GAME_PLATFORM_KEY);
                 String gamePublisher = bundle.getString(MediaKeys.GAME_PUBLISHER_KEY);
                 String gameSeries = bundle.getString(MediaKeys.GAME_PLATFORM_KEY);
                 String gameStoryline = bundle.getString(MediaKeys.GAME_STORYLINE_KEY);
 
+                // set the views of the xml layout to the attribute values
                 platform.setText(gamePlatform);
                 publisher.setText(gamePublisher);
                 series.setText(gameSeries);
