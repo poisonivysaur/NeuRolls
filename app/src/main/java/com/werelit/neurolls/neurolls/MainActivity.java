76c6130daab91c7b6c3deb55e4c39963229ea597
p01s0n.ivy <ivana_lim@dlsu.edu.ph>
2018-03-05 20:48:34 +0800

    added new search bar

--- app/src/main/java/com/werelit/neurolls/neurolls/MainActivity.java
+++ app/src/main/java/com/werelit/neurolls/neurolls/MainActivity.java
@@ -164,15 +164,15 @@ public class MainActivity extends AppCompatActivity
                 // do something
                 if (id == R.id.action_films) {
                     // Handle the home action
-                    Intent intent = new Intent(MainActivity.this, MediaSearchActivity.class);
+                    Intent intent = new Intent(MainActivity.this, SearchMediaActivity.class);
                     startActivity(intent);
                 } else if (id == R.id.action_books) {
                     //Toast.makeText(MainActivity.this , " Books clicked ", Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(MainActivity.this, MediaSearchActivity.class);
+                    Intent intent = new Intent(MainActivity.this, SearchMediaActivity.class);
                     startActivity(intent);
                 } else if (id == R.id.action_games) {
                     //Toast.makeText(MainActivity.this , " Games clicked ", Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(MainActivity.this, MediaSearchActivity.class);
+                    Intent intent = new Intent(MainActivity.this, SearchMediaActivity.class);
                     startActivity(intent);
                 }
             }
--- app/src/main/java/com/werelit/neurolls/neurolls/MediaSearchActivity.java
+++ app/src/main/java/com/werelit/neurolls/neurolls/MediaSearchActivity.java
@@ -82,6 +82,8 @@ public class MediaSearchActivity extends AppCompatActivity{
         }
     }
 
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
