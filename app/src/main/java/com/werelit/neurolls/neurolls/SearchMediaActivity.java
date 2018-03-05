a0a75bfbb053f01601eb2b1287e44326538e6903
p01s0n.ivy <ivana_lim@dlsu.edu.ph>
2018-03-05 21:07:27 +0800

    added auto open search view

--- app/src/main/java/com/werelit/neurolls/neurolls/SearchMediaActivity.java
+++ app/src/main/java/com/werelit/neurolls/neurolls/SearchMediaActivity.java
@@ -92,6 +92,7 @@ public class SearchMediaActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.search_menu, menu);
+        searchView.openSearch();
         return true;
     }
 
