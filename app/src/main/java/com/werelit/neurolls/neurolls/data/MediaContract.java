41d062ef739656bf1480049bf7ffff0db2c7f11a
p01s0n.ivy <ivana_lim@dlsu.edu.ph>
2018-03-05 21:45:16 +0800

    changed column names

--- app/src/main/java/com/werelit/neurolls/neurolls/data/MediaContract.java
+++ app/src/main/java/com/werelit/neurolls/neurolls/data/MediaContract.java
@@ -8,49 +8,49 @@ public class MediaContract {
         // Table name
         public static final String TABLE_NAME = "films";
 
-        public static final String COLUMN_YEAR_RELEASED = "year_released";
-        public static final String COLUMN_DIRECTOR = "director";
-        public static final String COLUMN_DURATION = "duration";
-        public static final String COLUMN_GENRE = "genre";
-        public static final String COLUMN_PRODUCTION = "production";
-        public static final String COLUMN_SYNOPSIS = "synopsis";
-        public static final String COLUMN_IMG_DIR = "image_directory";
-        public static final String COLUMN_ARCHIVED = "isArchived";
-        public static final String DATE_TO_WATCH = "date_to_watch";
-        public static final String NOTIF_SETTINGS = "notif_settings";
+        public static final String COLUMN_FILM_YEAR_RELEASED = "year_released";
+        public static final String COLUMN_FILM_DIRECTOR = "director";
+        public static final String COLUMN_FILM_DURATION = "duration";
+        public static final String COLUMN_FILM_GENRE = "genre";
+        public static final String COLUMN_FILM_PRODUCTION = "production";
+        public static final String COLUMN_FILM_SYNOPSIS = "synopsis";
+        public static final String COLUMN_FILM_IMG_DIR = "image_directory";
+        public static final String COLUMN_FILM_ARCHIVED = "isArchived";
+        public static final String COLUMN_FILMDATE_TO_WATCH = "date_to_watch";
+        public static final String COLUMN_FILM_NOTIF_SETTINGS = "notif_settings";
     }
 
     public static final class BookEntry implements BaseColumns{
         // Table name
         public static final String TABLE_NAME = "books";
 
-        public static final String COLUMN_YEAR_PUBLISHED = "year_published";
-        public static final String COLUMN_AUTHOR = "author";
-        public static final String COLUMN_PUBLISHER = "publisher";
-        public static final String COLUMN_GENRE = "genre";
-        public static final String COLUMN_DESCRIPTION = "description";
-        public static final String COLUMN_IMG_DIR = "image_directory";
-        public static final String COLUMN_ARCHIVED = "isArchived";
-        public static final String DATE_TO_READ = "date_to_read";
-        public static final String NOTIF_SETTINGS = "notif_settings";
+        public static final String COLUMN_BOOK_YEAR_PUBLISHED = "year_published";
+        public static final String COLUMN_BOOK_AUTHOR = "author";
+        public static final String COLUMN_BOOK_PUBLISHER = "publisher";
+        public static final String COLUMN_BOOK_GENRE = "genre";
+        public static final String COLUMN_BOOK_DESCRIPTION = "description";
+        public static final String COLUMN_BOOK_IMG_DIR = "image_directory";
+        public static final String COLUMN_BOOK_ARCHIVED = "isArchived";
+        public static final String COLUMN_BOOKDATE_TO_READ = "date_to_read";
+        public static final String COLUMN_BOOKNOTIF_SETTINGS = "notif_settings";
     }
 
     public static final class GameEntry implements BaseColumns{
         // Table name
         public static final String TABLE_NAME = "games";
 
-        public static final String COLUMN_YEAR_RELEASED = "year_released";
-        public static final String COLUMN_PLATFORM = "platform";
-        public static final String COLUMN_GENRE = "genre";
-        //public static final String COLUMN_COMPOSER = "composer";
-        //public static final String COLUMN_WRITER = "writer";
-        //public static final String COLUMN_DESIGNER = "designer";
-        public static final String COLUMN_PUBLISHER = "publisher";
-        public static final String COLUMN_SERIES = "series";
-        public static final String COLUMN_STORYLINE = "storyline";
-        public static final String COLUMN_IMG_DIR = "image_directory";
-        public static final String COLUMN_ARCHIVED = "isArchived";
-        public static final String DATE_TO_PLAY = "date_to_play";
-        public static final String NOTIF_SETTINGS = "notif_settings";
+        public static final String COLUMN_GAME_YEAR_RELEASED = "year_released";
+        public static final String COLUMN_GAME_PLATFORM = "platform";
+        public static final String COLUMN_GAME_GENRE = "genre";
+        //public static final String COLUMN_GAME_COMPOSER = "composer";
+        //public static final String COLUMN_GAME_WRITER = "writer";
+        //public static final String COLUM_GAMEN_DESIGNER = "designer";
+        public static final String COLUMN_GAME_PUBLISHER = "publisher";
+        public static final String COLUMN_GAME_SERIES = "series";
+        public static final String COLUMN_GAME_STORYLINE = "storyline";
+        public static final String COLUMN_GAME_IMG_DIR = "image_directory";
+        public static final String COLUMN_GAME_ARCHIVED = "isArchived";
+        public static final String COLUMN_GAMEDATE_TO_PLAY = "date_to_play";
+        public static final String COLUMN_GAMENOTIF_SETTINGS = "notif_settings";
     }
 }
