ca3556f9f4850b666797d5a8d2c43fc1ad8e6ab4
p01s0n.ivy <ivana_lim@dlsu.edu.ph>
2018-03-10 22:34:44 +0800

    added image key

--- app/src/main/java/com/werelit/neurolls/neurolls/MediaKeys.java
+++ app/src/main/java/com/werelit/neurolls/neurolls/MediaKeys.java
@@ -1,17 +1,19 @@
 package com.werelit.neurolls.neurolls;
 
 public class MediaKeys {
+    public final static String MEDIA_ID_KEY = "MEDIA_ID_KEY";
     public final static String MEDIA_NAME_KEY = "MEDIA_NAME_KEY";
     public final static String MEDIA_GENRE_KEY = "MEDIA_GENRE_KEY";
     public final static String MEDIA_YEAR_KEY = "MEDIA_YEAR_KEY";
-    public final static String MEDIA_ID_KEY = "MEDIA_ID_KEY";
+    public final static String MEDIA_IMAGE_KEY = "MEDIA_IMAGE_KEY";
 
-    public final static String FILM_DURATION_KEY = "FILM_DURATION_KEY";
     public final static String FILM_DIRECTOR_KEY = "FILM_DIRECTOR_KEY";
+    public final static String FILM_DURATION_KEY = "FILM_DURATION_KEY";
     public final static String FILM_PRODUCTION_KEY = "FILM_PRODUCTION_KEY";
     public final static String FILM_SYNOPSIS_KEY = "FILM_SYNOPSIS_KEY";
 
     public final static String BOOK_AUTHOR_KEY = "BOOK_AUTHOR_KEY";
+    public final static String BOOK_PAGES_KEY = "BOOK_PAGES_KEY";
     public final static String BOOK_PUBLISHER_KEY = "BOOK_PUBLISHER_KEY";
     public final static String BOOK_DESCRIPTION_KEY = "BOOK_DESCRIPTION_KEY";
 
