c5e218c7280357d24a4b23950e96408b9d8b9f7c
p01s0n.ivy <ivana_lim@dlsu.edu.ph>
2018-03-10 14:27:50 +0800

    organized columns

--- app/src/main/java/com/werelit/neurolls/neurolls/data/MediaContract.java
+++ app/src/main/java/com/werelit/neurolls/neurolls/data/MediaContract.java
@@ -10,12 +10,14 @@ public class MediaContract {
 
         public static final String COLUMN_FILM_ID = "film_id";
         public static final String COLUMN_FILM_NAME = "film_name";
+        public static final String COLUMN_FILM_GENRE = "genre";
         public static final String COLUMN_FILM_YEAR_RELEASED = "year_released";
+
         public static final String COLUMN_FILM_DIRECTOR = "director";
         public static final String COLUMN_FILM_DURATION = "duration";
-        public static final String COLUMN_FILM_GENRE = "genre";
         public static final String COLUMN_FILM_PRODUCTION = "production";
         public static final String COLUMN_FILM_SYNOPSIS = "synopsis";
+
         public static final String COLUMN_FILM_IMG_DIR = "image_directory";
         public static final String COLUMN_FILM_ARCHIVED = "isArchived";
         public static final String COLUMN_FILM_DATE_TO_WATCH = "date_to_watch";
@@ -29,11 +31,13 @@ public class MediaContract {
 
         public static final String COLUMN_BOOK_ID = "book_id";
         public static final String COLUMN_BOOK_NAME = "book_name";
+        public static final String COLUMN_BOOK_GENRE = "genre"; // this will become page number
         public static final String COLUMN_BOOK_YEAR_PUBLISHED = "year_published";
+
         public static final String COLUMN_BOOK_AUTHOR = "author";
         public static final String COLUMN_BOOK_PUBLISHER = "publisher";
-        public static final String COLUMN_BOOK_GENRE = "genre";
         public static final String COLUMN_BOOK_DESCRIPTION = "description";
+
         public static final String COLUMN_BOOK_IMG_DIR = "image_directory";
         public static final String COLUMN_BOOK_ARCHIVED = "isArchived";
         public static final String COLUMN_BOOK_DATE_TO_READ = "date_to_read";
@@ -47,12 +51,14 @@ public class MediaContract {
 
         public static final String COLUMN_GAME_ID = "game_id";
         public static final String COLUMN_GAME_NAME = "game_name";
-        public static final String COLUMN_GAME_YEAR_RELEASED = "year_released";
-        public static final String COLUMN_GAME_PLATFORM = "platform";
         public static final String COLUMN_GAME_GENRE = "genre";
+        public static final String COLUMN_GAME_YEAR_RELEASED = "year_released";
+
+        public static final String COLUMN_GAME_PLATFORM = "platform";
         public static final String COLUMN_GAME_PUBLISHER = "publisher";
         public static final String COLUMN_GAME_SERIES = "series";
         public static final String COLUMN_GAME_STORYLINE = "storyline";
+
         public static final String COLUMN_GAME_IMG_DIR = "image_directory";
         public static final String COLUMN_GAME_ARCHIVED = "isArchived";
         public static final String COLUMN_GAME_DATE_TO_PLAY = "date_to_play";
