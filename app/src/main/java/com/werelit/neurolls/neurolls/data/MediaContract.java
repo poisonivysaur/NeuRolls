0e7b0be2521876b1842d7ce1fc80e71b7f327dad
p01s0n.ivy <ivana_lim@dlsu.edu.ph>
2018-03-10 14:31:11 +0800

    organized column order

--- app/src/main/java/com/werelit/neurolls/neurolls/data/MediaContract.java
+++ app/src/main/java/com/werelit/neurolls/neurolls/data/MediaContract.java
@@ -19,10 +19,10 @@ public class MediaContract {
         public static final String COLUMN_FILM_SYNOPSIS = "synopsis";
 
         public static final String COLUMN_FILM_IMG_DIR = "image_directory";
-        public static final String COLUMN_FILM_ARCHIVED = "isArchived";
         public static final String COLUMN_FILM_DATE_TO_WATCH = "date_to_watch";
-        public static final String COLUMN_FILM_WATCHED = "isWatched";
         public static final String COLUMN_FILM_NOTIF_SETTINGS = "notif_settings";
+        public static final String COLUMN_FILM_WATCHED = "isWatched";
+        public static final String COLUMN_FILM_ARCHIVED = "isArchived";
     }
 
     public static final class BookEntry implements BaseColumns{
@@ -39,10 +39,10 @@ public class MediaContract {
         public static final String COLUMN_BOOK_DESCRIPTION = "description";
 
         public static final String COLUMN_BOOK_IMG_DIR = "image_directory";
-        public static final String COLUMN_BOOK_ARCHIVED = "isArchived";
         public static final String COLUMN_BOOK_DATE_TO_READ = "date_to_read";
-        public static final String COLUMN_FILM_READ = "isRead";
         public static final String COLUMN_BOOK_NOTIF_SETTINGS = "notif_settings";
+        public static final String COLUMN_FILM_READ = "isRead";
+        public static final String COLUMN_BOOK_ARCHIVED = "isArchived";
     }
 
     public static final class GameEntry implements BaseColumns{
@@ -60,9 +60,9 @@ public class MediaContract {
         public static final String COLUMN_GAME_STORYLINE = "storyline";
 
         public static final String COLUMN_GAME_IMG_DIR = "image_directory";
-        public static final String COLUMN_GAME_ARCHIVED = "isArchived";
         public static final String COLUMN_GAME_DATE_TO_PLAY = "date_to_play";
-        public static final String COLUMN_FILM_PLAYED = "isPlayed";
         public static final String COLUMN_GAME_NOTIF_SETTINGS = "notif_settings";
+        public static final String COLUMN_FILM_PLAYED = "isPlayed";
+        public static final String COLUMN_GAME_ARCHIVED = "isArchived";
     }
 }
