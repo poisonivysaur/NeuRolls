4e6387784de5d29b96bafb0223912e0f2582f81b
p01s0n.ivy <ivana_lim@dlsu.edu.ph>
2018-03-12 11:55:19 +0800

    nevermind the renaming

--- app/src/main/java/com/werelit/neurolls/neurolls/data/MediaProvider.java
+++ app/src/main/java/com/werelit/neurolls/neurolls/data/MediaProvider.java
@@ -8,10 +8,10 @@ import android.net.Uri;
 /**
  * {@link ContentProvider} for Medias app.
  */
-public class NeurollsProvider extends ContentProvider {
+public class MediaProvider extends ContentProvider {
 
     /** Tag for the log messages */
-    public static final String LOG_TAG = NeurollsProvider.class.getSimpleName();
+    public static final String LOG_TAG = MediaProvider.class.getSimpleName();
 
     private NeurollsDbHelper mDbHelper;
 
