b8497f5b3dfaa9632affd0e861e0cc58b5e4f7c7
p01s0n.ivy <ivana_lim@dlsu.edu.ph>
2018-04-01 20:46:40 +0800

    Revert "deleted string to bitmap method"
    This reverts commit 30fb3d54df4f803527d4072a7bd853810b2ad5f6.

--- app/src/main/java/com/werelit/neurolls/neurolls/network/JsonConverter.java
+++ app/src/main/java/com/werelit/neurolls/neurolls/network/JsonConverter.java
@@ -268,4 +268,19 @@ public class JsonConverter {
             }
         }
     }
+
+    /**
+     * @param encodedString
+     * @return bitmap (from given string)
+     */
+    public static Bitmap stringToBitMap(String encodedString){
+        try {
+            byte [] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch(Exception e) {
+            e.getMessage();
+            return null;
+        }
+    }
 }
